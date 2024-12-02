435efc7b338f1ec3e5ce88571da84ffe3e5ce766
Acacio de Lima <limadeacacio@gmail.com>
2024-12-02 13:18:17 -0300

    refactor: improve layout structure in HomePage component for better spacing and organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,33 +18,31 @@ export default function HomePage() {
   } = useFabric();
 
   return (
-    <main className="w-full min-h-screen h-full flex items-center justify-center">
-      <div className="flex items-center flex-col space-y-6">
-        <canvas
-          ref={canvasRef}
-          className="border rounded-3xl overflow-hidden mx-auto"
-        />
+    <main className="w-full min-h-screen h-full py-12 flex flex-col items-center justify-center space-y-8 mx-auto">
+      <canvas
+        ref={canvasRef}
+        className="border rounded-3xl overflow-hidden mx-auto"
+      />
 
-        <Toolbar
-          setBackgroundImage={setBackgroundImage}
-          addText={addText}
-          addChillGuy={addChillGuy}
-          flipImage={flipImage}
-          deleteSelectedObject={deleteSelectedObject}
-          downloadCanvas={downloadCanvas}
-          changeBackgroundColor={changeBackgroundColor}
-          currentBackgroundColor={currentBackgroundColor}
-          canFlipDelete={canFlipDelete}
-        />
+      <Toolbar
+        setBackgroundImage={setBackgroundImage}
+        addText={addText}
+        addChillGuy={addChillGuy}
+        flipImage={flipImage}
+        deleteSelectedObject={deleteSelectedObject}
+        downloadCanvas={downloadCanvas}
+        changeBackgroundColor={changeBackgroundColor}
+        currentBackgroundColor={currentBackgroundColor}
+        canFlipDelete={canFlipDelete}
+      />
 
-        <a
-          className="text-sm text-muted-foreground "
-          href="https://github.com/limazia"
-          target="_blank"
-        >
-          criado por @limazia
-        </a>
-      </div>
+      <a
+        className="text-sm text-muted-foreground"
+        href="https://github.com/limazia"
+        target="_blank"
+      >
+        criado por @limazia
+      </a>
     </main>
   );
 }
